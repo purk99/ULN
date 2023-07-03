4743b263b3a40c3b86b07a35dc173b0c5f7be38b
TheXecution <utfaj@student.kit.edu>
2023-07-03 22:29:17 +0200

    initial addition of stuff

--- ULN/src/main.cpp
+++ ULN/src/main.cpp
@@ -1,18 +1,15 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Blink.h>
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  blinkSetup();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  blinkLoop();
+
 }
